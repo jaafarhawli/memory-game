63f3d0490190219aa36472e772b59b2abc78f696
JaafarHawli <jaafarhawle@gmail.com>
2022-09-16 19:55:00 +0300

    added scoring system

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,8 @@ let card2;
 let firstClick = true;
 let lock = false;
 let score = 0;
+let flips = 0;
+let cardsLeft = 6;
 
 (function shuffle() {
 	card.forEach((item) => {
@@ -18,12 +20,13 @@ let score = 0;
 card.forEach((card) => card.addEventListener('click', flip));
 
 function flip() {
-	if (this === card1) {
+	if (this == card1) {
 		return;
 	}
 	if (lock == true) {
 		return;
 	}
+	flips += 1;
 	if (firstClick) {
 		card1 = this;
 		firstClick = false;
@@ -32,8 +35,20 @@ function flip() {
 	} else {
 		card2 = this;
 		this.classList.add('flip-card');
-		if (card1.dataset.framework === card2.dataset.framework) {
-			statusText.innerHTML = 'Matched';
+		if (card1.dataset.framework == card2.dataset.framework) {
+			cardsLeft -= 2;
+			if (cardsLeft > 0) {
+				statusText.innerHTML = 'Matched';
+			} else {
+				score = 10 - (flips - 10);
+				if (score < 0) {
+					score = 0;
+				}
+				if (score > 10) {
+					score = 10;
+				}
+				statusText.innerHTML = `Congrats! Your score = ${score}`;
+			}
 			lock = true;
 			setTimeout(() => {
 				card1.classList.toggle('flip-card');
@@ -50,8 +65,6 @@ function flip() {
 		} else {
 			lock = true;
 			setTimeout(() => {
-				/* card1.classList.toggle('flip-card');
-				card1.classList.toggle('flip-card'); */
 				card1.classList.remove('flip-card');
 				card2.classList.remove('flip-card');
 				statusText.innerHTML = 'Try again';
@@ -65,6 +78,9 @@ function flip() {
 }
 
 reset.addEventListener('click', () => {
+	if (flips == 0) {
+		return;
+	}
 	card.forEach((element) => {
 		if (element.style.opacity == 0) {
 			element.style.opacity = 1;
@@ -75,5 +91,9 @@ reset.addEventListener('click', () => {
 	card1 = null;
 	card2 = null;
 	lock = false;
+	cardsLeft = 6;
+	flips = 0;
+	score = 0;
+	statusText.innerHTML = 'Start';
 	card.forEach((card) => card.addEventListener('click', flip));
 });
